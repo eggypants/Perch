045dec07e1d927dca959a69d166f7ca988833a83
eggypants <lindseyvm@gmail.com>
2026-07-03 12:40:58 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE = "perch-v1";
+const CACHE = "perch-v2";
 const ASSETS = [
   "./", "./index.html", "./manifest.json",
   "./icon-180.png", "./icon-192.png", "./icon-512.png",
   "https://cdnjs.cloudflare.com/ajax/libs/react/18.2.0/umd/react.production.min.js",
   "https://cdnjs.cloudflare.com/ajax/libs/react-dom/18.2.0/umd/react-dom.production.min.js",
   "https://cdnjs.cloudflare.com/ajax/libs/prop-types/15.8.1/prop-types.min.js",
-  "https://unpkg.com/recharts@2.12.7/umd/Recharts.min.js",
+  "https://cdn.jsdelivr.net/npm/recharts@2.12.7/umd/Recharts.min.js",
   "https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/7.23.5/babel.min.js"
 ];
 self.addEventListener("install", (e) => {
